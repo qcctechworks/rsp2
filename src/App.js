277993567bd1acf37e1e19dabf81512cb70cf72b
unknown <qcctechworks@gmail.com>
2020-02-27 20:34:13 -0800

    Added the decideWinner method & playGame method - tested and working yay :)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PlayerCard from './PlayerCard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Game extends Component {
+  constructor() {
+    super();
+    this.signs = ["rock","scissors","paper"];
+    this.state = {
+      playerOne: "rock",
+      playerTwo: "scissors",
+    }
+  }
+
+//this is where my methods live yay :)
+ playGame = () => {
+  this.setState({
+    playerOne: this.signs[Math.floor(Math.random()* 3)],
+    playerTwo: this.signs[Math.floor(Math.random()* 3)],
+  })
 }
 
-export default App;
+decideWinner = () => {
+  const playerOne = this.state.playerOne;
+  const playerTwo = this.state.playerTwo;
+
+  if(playerOne === playerTwo) {
+    return "It's a Tie!!"
+  } else if ((playerOne === "rock" && playerTwo === "scissors") ||
+             (playerOne === "scissors" && playerTwo === "paper")||
+             (playerOne === "paper" && playerTwo === "rock")) {
+               return "Player 1 Wins!!"
+  } else {
+      return "Player Two Wins!!"
+  }
+}
+
+
+  render() {
+    return(
+      <div className="container">
+        <div>
+        <PlayerCard sign={this.state.playerOne} />
+        <PlayerCard sign={this.state.playerTwo}/>
+        </div>
+        <div className="winner">{this.decideWinner()}</div>
+        <button type="button" onClick={this.playGame}>Play the Game</button>
+      </div>
+    )
+  }
+};
+
+export default Game;
---
+++ src/PlayerCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const scissors = "https://i.imgur.com/pgjyhIZ.png";
+const rock = "https://i.imgur.com/LghSkIw.png";
+const paper = "https://i.imgur.com/2gsdqvR.png";
+
+const PlayerCard = (props) => {
+  const sign = props.sign;
+  let image = "";
+
+  if(sign === "rock"){
+    image = rock;
+  } else if (sign === "paper") {
+    image = paper;
+  } else {
+    image = scissors
+  }
+
+  return (
+    <div className="player-card">
+      <img src={image} />
+    </div>
+  )
+};
+
+export default PlayerCard
